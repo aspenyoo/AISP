253f1c2ee578ea9bb5fd2f0b193c09efb62f5d04
aspenyoo <aspen.yoo@nyu.edu>
2020-06-16 12:04:29 -0400

    freq and bayes fits

---
+++ detectionAspen/calculate_optimaldecisioncriteria.m
@@ -0,0 +1,117 @@
+function dec_rule = calculate_optimaldecisioncriteria(pars)
+
+persistent optimal_criterion
+persistent saved_pars
+persistent k_range
+persistent J_lin
+persistent highest_J
+
+Jbar_high = pars(1);
+Jbar_low = pars(2);
+tau = pars(3);
+
+nItems = 4;
+nLowVec = nItems:-1:0;
+nRelConds = length(nLowVec);
+
+if isempty(saved_pars) || any(saved_pars ~=pars)
+    
+    % make CDF for interpolating J to Kappa
+    if isempty(k_range)
+        tempp = load('cdf_table.mat');
+        % K_interp = tempp.K_interp;
+        % cdf = tempp.cdf;
+        k_range = tempp.k_range;
+        J_lin = tempp.J_lin;
+        highest_J = tempp.highest_J;
+    end
+    
+    optimal_criterion = zeros(1,nRelConds);
+    nSim = 1e5; % half of this will make up d0 and d1
+    for icond = 1:nRelConds
+        icond
+        nLow = nLowVec(icond);
+        
+        % mean precision, Jbar
+        Jbar_mat = nan(nSim,nItems);
+        Jbar_mat(:,1:nLow) = Jbar_low;
+        Jbar_mat(:,(nLow+1):nItems) = Jbar_high;
+        
+        % sampled precision, J
+        J_x_mat = gamrnd(Jbar_mat./tau,tau);
+        J_y_mat = gamrnd(Jbar_mat./tau,tau);
+        % set kappas too high to highest J (alternatively can resample, as
+        % keshvari did)
+        J_x_mat(J_x_mat > highest_J) = highest_J;
+        J_y_mat(J_y_mat > highest_J) = highest_J;
+        
+        % convert J to kappa
+        xi = 1/diff(J_lin(1:2))*J_x_mat+1;
+        kappa_x_i = k_range(round(xi));
+        xi = 1/diff(J_lin(1:2))*J_y_mat+1;
+        kappa_y_i = k_range(round(xi));
+        
+        if size(kappa_x_i,2) ~= nItems
+            kappa_x_i = kappa_x_i';
+            kappa_y_i = kappa_y_i';
+        end
+        
+        % generate measurement noise
+        noise_x = circ_vmrnd(0,kappa_x_i);
+        noise_y = circ_vmrnd(0,kappa_y_i);
+        
+        % get difference between noise
+        delta_noise = noise_x-noise_y;
+        Delta = zeros(nSim/2,4);
+        Delta = [Delta; 4*pi.*rand(nSim/2,4)-2*pi];
+        
+        % the term inside denominator bessel function for d_i
+        Kc = bsxfun(@times,2.*kappa_x_i.*kappa_y_i,cos(bsxfun(@plus,Delta,delta_noise))); % note: it is okay to simply add the noise bc it goes through a cos!!
+        Kc = sqrt(bsxfun(@plus,kappa_x_i.^2+kappa_y_i.^2,Kc)); % dims: mat_dims
+        
+        % d1 d0
+        d_Mat = squeeze(max(kappa_x_i + kappa_y_i - Kc,[],2));
+        d0 = d_Mat(1:nSim/2,:);
+        d1 = d_Mat(nSim/2+1:end,:);
+        
+        % Bisection method homebrew:
+        crit0 = -20;
+        crit2 = 100;
+        crit1 = crit0 + (crit2-crit0) / ( 3 + sqrt(5) ) * 2;
+        f0 = sum(d1 < crit0) + sum(d0 > crit0);
+        f1 = sum(d1 < crit1) + sum(d0 > crit1);
+        f2 = sum(d1 < crit2) + sum(d0 > crit2);
+        while (crit2-crit0) > 0.001
+            if abs(crit2-crit1) > abs(crit1-crit0)
+                crit_new = crit1 + (crit2-crit1) / ( 3 + sqrt(5) ) * 2
+                f_new = sum(d1 < crit_new) + sum(d0 > crit_new)
+                if f_new < f1
+                    f2 = f_new;
+                    crit2 = crit_new
+                else
+                    f0 = f1;
+                    crit0 = crit1;
+                    f1 = f_new;
+                    crit1 = crit_new
+                end
+            else
+                crit_new = crit1 + (crit0-crit1) / ( 3 + sqrt(5) ) * 2
+                f_new = sum(d1 < crit_new) + sum(d0 > crit_new)
+                if f_new < f1
+                    f0 = f_new;
+                    crit0 = crit_new
+                else
+                    f2 = f1;
+                    crit2 = crit1;
+                    f1 = f_new;
+                    crit1 = crit_new
+                end
+            end
+        end
+        optimal_criterion(icond) = crit1;
+        
+    end
+    saved_pars = pars;
+end
+dec_rule = optimal_criterion;
+
--- detectionAspen/simulate_responses.m
+++ detectionAspen/simulate_responses.m
@@ -1,7 +1,7 @@
 function resp = simulate_responses(x,model,dMat,logflag)
 %function RESP = simulate_responses(X,MODEL,DMAT,LOGFLAG) simulates responses of
 %bayesian observer
-% 
+%
 % ============ INPUT VARIABLES ============
 % X: parameter values. vector of length six
 %       'bayes': [Jbar_high, Jbar_low, tau, sigma_d, p_change, lambda]
@@ -13,10 +13,10 @@ function resp = simulate_responses(x,model,dMat,logflag)
 % DMAT: data. nTrials x 8 matrix
 %       first four columns correspond to amount of orientation change for
 %       each item. second four columns correspond to the reliability of
-%       each item. 
+%       each item.
 % LOGFLAG: log flag. binary vector of length six
 %       indicates which parameters are in log scaling
-% 
+%
 % ============ OUTPUT VARIABLES ============
 % RESP: 1xnTrials vector of simulated responses
 
@@ -66,42 +66,36 @@ if sum(~islapse) % if there are any trials that did not lapse
         highest_J = tempp.highest_J;
     end
     
-%     % calculate actual kappa and noisy representations
-%     [delta_noise, kappa_x_i, kappa_y_i] = generate_representations;
-%this function simulates kappas and xi-yi
-        %
-        % ======= OUTPUT VARIABLE =====
-        % DELTA_NOISE: matrix of dimension [nTrials,nItems,nSamples]
-        
-        Jbar_mat = Rels;
-        Jbar_mat(Rels==1) = Jbar_low;
-        Jbar_mat(Rels==2) = Jbar_high;
-        
-        J_x_mat = gamrnd(Jbar_mat./tau,tau);
-        J_y_mat = gamrnd(Jbar_mat./tau,tau);
-        
-        % set kappas too high to highest J (alternatively can resample, as
-        % keshvari did)
-        J_x_mat(J_x_mat > highest_J) = highest_J;
-        J_y_mat(J_y_mat > highest_J) = highest_J;
-        
-        % convert J to kappa
-        xi = 1/diff(J_lin(1:2))*J_x_mat+1;
-        kappa_x_i = k_range(round(xi));
-        xi = 1/diff(J_lin(1:2))*J_y_mat+1;
-        kappa_y_i = k_range(round(xi));
-        
-        if size(kappa_x_i,2) ~= nItems
-            kappa_x_i = kappa_x_i';
-            kappa_y_i = kappa_y_i';
-        end
-        
-        % generate measurement noise
-        noise_x = circ_vmrnd(0,kappa_x_i);
-        noise_y = circ_vmrnd(0,kappa_y_i);
-        
-        % get difference between noise
-        delta_noise = noise_x-noise_y;
+    % calculate actual kappa and noisy representations
+    Jbar_mat = Rels;
+    Jbar_mat(Rels==1) = Jbar_low;
+    Jbar_mat(Rels==2) = Jbar_high;
+    
+    J_x_mat = gamrnd(Jbar_mat./tau,tau);
+    J_y_mat = gamrnd(Jbar_mat./tau,tau);
+    
+    % set kappas too high to highest J (alternatively can resample, as
+    % keshvari did)
+    J_x_mat(J_x_mat > highest_J) = highest_J;
+    J_y_mat(J_y_mat > highest_J) = highest_J;
+    
+    % convert J to kappa
+    xi = 1/diff(J_lin(1:2))*J_x_mat+1;
+    kappa_x_i = k_range(round(xi));
+    xi = 1/diff(J_lin(1:2))*J_y_mat+1;
+    kappa_y_i = k_range(round(xi));
+    
+    if size(kappa_x_i,2) ~= nItems
+        kappa_x_i = kappa_x_i';
+        kappa_y_i = kappa_y_i';
+    end
+    
+    % generate measurement noise
+    noise_x = circ_vmrnd(0,kappa_x_i);
+    noise_y = circ_vmrnd(0,kappa_y_i);
+    
+    % get difference between noise
+    delta_noise = noise_x-noise_y;
     
     % the term inside denominator bessel function for d_i
     Kc = bsxfun(@times,2.*kappa_x_i.*kappa_y_i,cos(bsxfun(@plus,Delta,delta_noise))); % note: it is okay to simply add the noise bc it goes through a cos!!
@@ -112,59 +106,16 @@ if sum(~islapse) % if there are any trials that did not lapse
         case 'bayes'
             d_i_Mat = bsxfun(@minus,log(besseli(0,kappa_x_i,1).*besseli(0,kappa_y_i,1))+...
                 (kappa_x_i+kappa_y_i),log(besseli(0,Kc,1))+Kc); % actually log d_i_Mat
-            
-            p_C_hat = log(sum(exp(d_i_Mat),2))-log(nItems);  % these values are actually log(d), not p_C_hat
+            p_C_hat = log(sum(exp(d_i_Mat),2))-log(nItems)+log(k)-log(1-k);  % these values are actually log(d), not p_C_hat
+%             p_C_hat = log(sum(exp(d_i_Mat),2))-log(nItems);  % these values are actually log(d), not p_C_hat
             p_C_hat = p_C_hat + randn(size(p_C_hat)).*sigma_d;    % global dec noise
-            p_C_hat = p_C_hat > k; %1;      % respond 1 if log(d) > log(1)
-        case 'freq'
+            p_C_hat = p_C_hat > 0; %k     % respond 1 if log(d) > log(1)
+        case 'freq' % point estimate model
             d_Mat = squeeze(max(kappa_x_i + kappa_y_i - Kc,[],2));
             p_C_hat = d_Mat > k; % respond 1 if bias term
+        case 'freq2' % optimal point estimate model
     end
     resp(~islapse) = p_C_hat;
-    
-% %     fprintf('blah')
-% %     size(resp)
-% %     size(dMat)
 end
 
-% ================================================================
-%                      HELPER FUNCTIONS
-% % ================================================================
-%     function [delta_noise, kappa_x, kappa_y] = generate_representations
-%         %this function simulates kappas and xi-yi
-%         %
-%         % ======= OUTPUT VARIABLE =====
-%         % DELTA_NOISE: matrix of dimension [nTrials,nItems,nSamples]
-%         
-%         Jbar_mat = Rels;
-%         Jbar_mat(Rels==1) = Jbar_low;
-%         Jbar_mat(Rels==2) = Jbar_high;
-%         
-%         J_x_mat = gamrnd(Jbar_mat./tau,tau);
-%         J_y_mat = gamrnd(Jbar_mat./tau,tau);
-%         
-%         % set kappas too high to highest J (alternatively can resample, as
-%         % keshvari did)
-%         J_x_mat(J_x_mat > highest_J) = highest_J;
-%         J_y_mat(J_y_mat > highest_J) = highest_J;
-%         
-%         % convert J to kappa
-%         xi = 1/diff(J_lin(1:2))*J_x_mat+1;
-%         kappa_x = k_range(round(xi));
-%         xi = 1/diff(J_lin(1:2))*J_y_mat+1;
-%         kappa_y = k_range(round(xi));
-%         
-%         if size(kappa_x,2) ~= nItems
-%             kappa_x = kappa_x';
-%             kappa_y = kappa_y';
-%         end
-%         
-%         % generate measurement noise
-%         noise_x = circ_vmrnd(0,kappa_x);
-%         noise_y = circ_vmrnd(0,kappa_y);
-%         
-%         % get difference between noise
-%         delta_noise = noise_x-noise_y;
-%         
-%     end
 end
