ea60436bee665d6f0f507c348223fa20027bf8e5
aspenyoo <aspen.yoo@nyu.edu>
2020-07-01 09:31:38 -0400

    pe2 fits from cluster

--- detectionAspen/fit_parameters.s
+++ detectionAspen/fit_parameters.s
@@ -2,8 +2,8 @@
 #
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=01:00:00
-#SBATCH --mem=4GB
+#SBATCH --time=03:00:00
+#SBATCH --mem=7GB
 #SBATCH --job-name=fit_parameters
 #SBATCH --mail-type=END
 #SBATCH --mail-user=aspen.yoo@nyu.edu
