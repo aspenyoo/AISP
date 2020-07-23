ac099a55c0ea4d0e2bb9ea88d06a218434e3b59c
aspenyoo <aspen.yoo@nyu.edu>
2020-07-23 10:35:43 -0400

    updating fit_parameters.s to complete idxs that terminated early

--- detectionAspen/fit_parameters.s
+++ detectionAspen/fit_parameters.s
@@ -2,12 +2,12 @@
 #
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=3:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mem=7GB
-#SBATCH --job-name=fit_parameters
+#SBATCH --job-name=refit_idx
 #SBATCH --mail-type=END
 #SBATCH --mail-user=aspen.yoo@nyu.edu
-#SBATCH --output=o_%a.out
+#SBATCH --output=re_%a.out
 
 module purge
 module load matlab/2016b
@@ -17,14 +17,15 @@ addpath(genpath('/home/ay963/matlab-scripts'))
 addpath(genpath('/home/ay963/bigAISP/detectionAspen'))
 
 % fixed model fitting settings
-nReps = 20;
-idxx = $SLURM_ARRAY_TASK_ID;
 load('idxstocomplete.mat')
-idx=idxlist(idxx);
+runs = csvread('runs.csv');
 
+idxx = $SLURM_ARRAY_TASK_ID;
+idx = idxlist(idxx);
+imodel = runs(idx, 3);
+isubj = runs(idx, 1);
+irep = runs(idx, 2);
 
-fit_cluster_ibs(idx,nReps)
-
-blah
+fit_cluster_ibs(imodel,isubj,irep)
 
 EOF
